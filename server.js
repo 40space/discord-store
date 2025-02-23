5316303fea3f8b536bbb73d2a9d2a388da0d3ef0
40space <spacexstore123@gmail.com>
2025-02-23 21:37:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ app.use(express.json());
 let discordUserId = null;
 
 app.get('/auth/discord', (req, res) => {
-    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=http://localhost:3000/auth/discord/callback&response_type=code&scope=identify`);
+    res.redirect('https://discord.com/oauth2/authorize?client_id=1343301532996862087&response_type=code&redirect_uri=https%3A%2F%2F4bspace.github.io%2Fdiscord-store%2Fauth%2Fdiscord%2Fcallback&scope=identify+guilds.join');
 });
 
 app.get('/auth/discord/callback', async (req, res) => {
     const code = req.query.code;
     const response = await axios.post('https://discord.com/api/oauth2/token', new URLSearchParams({
         client_id: '1343301532996862087',
-        client_secret: 'sctUoQ6Wy6pMBm4f6Vc_exCnolKavMa5',
+        client_secret: 'sctUoQ6Wy6pMBm4f6Vc_exCnolKavMa5', // استبدل بـ Client Secret الخاص بك
         grant_type: 'client_credentials',
         code: code,
-        redirect_uri: 'http://localhost:3000/auth/discord/callback',
-        scope: 'identify'
+        redirect_uri: 'https://4bspace.github.io/discord-store/auth/discord/callback',
+        scope: 'identify guilds.join'
     }), {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
